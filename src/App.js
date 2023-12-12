b397df4bb8e00beece02721e1d13a6a4f01e2f18
AyusKum <officialidayush@gmail.com>
2023-12-12 23:38:51 +0530

    day2 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import {BodyCard} from './components/BodyCard';
+import './components/TransparentBG.css'
 
 export class App extends Component {
   render() {
     return (
       <div>
-      <Navbar/>
+      <Navbar className="opaque"/>
+      <img src="https://media.istockphoto.com/id/1190043570/photo/happy-wedding-photography-of-bride-and-groom-at-wedding-ceremony-wedding-tradition-sprinkled.jpg?s=612x612&w=0&k=20&c=_aCIW5-iOIiaDdqin_50kvBcbFbIxSULHHamPUILE0c=" className="lol">
+      </img>
       <BodyCard/>
+      
       </div>
     )
   }
--- src/components/BodyCard.js
+++ src/components/BodyCard.js
@@ -3,6 +3,7 @@ import Ca from "./Ca";
 
 
 export class BodyCard extends Component {
+  
   articles= [
     {
       "source": {
@@ -11,7 +12,7 @@ export class BodyCard extends Component {
       "title": "Goa",
       "description": "A popular and fun-filled destination, Goa has many resorts and beaches that can host a lively and memorable wedding.",
       "url": "/",
-      "urlToImage": "src/components/Goa.jpg"
+      "urlToImage": "https://plus.unsplash.com/premium_photo-1697729435209-97772693b7da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z29hfGVufDB8fDB8fHww"
     },
     {
       "source": {
@@ -20,7 +21,7 @@ export class BodyCard extends Component {
       "title": "Udaipur",
       "description": "The city of lakes and palaces, Udaipur offers a royal and romantic setting for a destination wedding",
       "url": "/u",
-      "urlToImage":"src/components/udaipur.jpg"
+      "urlToImage":"https://plus.unsplash.com/premium_photo-1697730426227-9056296a0315?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHVkYWlwdXJ8ZW58MHx8MHx8fDA%3D"
     },
     {
       "source": {
@@ -29,7 +30,7 @@ export class BodyCard extends Component {
       "title": "Kerela",
       "description": "For a serene and scenic wedding, Kerala is a great option.Kerala also offers a rich culture and cuisine that will add to the charm of your wedding14.",
       "url": "/k",
-      "urlToImage": "src/components/DoubleTree-by-Hilton-Hotel-Goa.jpg"
+      "urlToImage": "https://images.unsplash.com/photo-1603680887147-dceacf6e19e5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGtlcmVsYXxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
       "source": {
@@ -38,7 +39,7 @@ export class BodyCard extends Component {
       "title": "Andaman and Nicobar Islands",
       "description": "If you want a tropical and exotic wedding, the Andaman and Nicobar Islands are a dream destination",
       "url": "/a",
-      "urlToImage": "src/components/andaman.jpg"
+      "urlToImage": "https://images.unsplash.com/photo-1617653202545-931490e8d7e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YW5kYW1hbiUyMGFuZCUyMG5pY29iYXIlMjBpc2xhbmRzfGVufDB8fDB8fHww"
     },
     {
       "source": {
@@ -48,7 +49,7 @@ export class BodyCard extends Component {
       "title": "Agra",
       "description": "If you want to celebrate your love in the city of love, Agra is the perfect place for you. You can have a romantic wedding in front of the Taj Mahal, the symbol of eternal love",
       "url": "/ag",
-      "urlToImage":"src/components/agra.jpg"
+      "urlToImage":"https://images.unsplash.com/photo-1612810436541-336b73fbcf9f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YWdyYXxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
       "source": {
@@ -57,7 +58,7 @@ export class BodyCard extends Component {
       "title": "Jaisalmer",
       "description": "For a desert-themed wedding, Jaisalmer is a unique and stunning choice. You can get married in the golden sand dunes, under the starry sky, or in the majestic forts and havelis that reflect the rich culture and heritage of Rajasthan.",
       "url": "/ja",
-      "urlToImage": "src/components/andaman.jpg"
+      "urlToImage": "https://media.istockphoto.com/id/1303019513/photo/various-views-of-the-jaiselmer-fort.webp?b=1&s=170667a&w=0&k=20&c=T4be0nutKV3FK3Lqr6eXpp97EkyMUd-efiV050X9pXA="
     },
     {
       "source": {
@@ -66,7 +67,7 @@ export class BodyCard extends Component {
       "title": "Mussoorie",
       "description": "The Doon Valley, the majestic Himalayas, and a brilliant night sky filled with dazzling stars provide the perfect setting for your D-Day.",
       "url": "/mu",
-      "urlToImage":"C:\Users\Asus\Desktop\marriage\src\components\agra.jpg"
+      "urlToImage":"https://images.unsplash.com/photo-1583143874828-de3d288be51a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bXVzc29vcmllfGVufDB8fDB8fHww"
       
     }
   ]
